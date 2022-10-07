8fef73265c990bb8efee45377b8aac1ce9824919
theshenron <Gourav143faz@gmail.com>
2022-10-07 16:55:22 +0530

    done

--- script/main.js
+++ script/main.js
@@ -1,2 +1,5 @@
 console.log("From main.js file .. Welcome")
 // logotype or something with text 
+document.querySelector("#hamIcon").addEventListener("click" , ()=>{
+    document.querySelector(".menu_sidebar").classList.toggle("show")
+})
