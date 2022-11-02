5398f26c58447a440af025deb3c7c8bba00253ce
theshenron <gourav143faz@gmail.com>
2022-11-02 19:58:51 +0530

    updating...

--- script/main.js
+++ script/main.js
@@ -6,33 +6,33 @@ document.querySelector("#hamIcon").addEventListener("click" , ()=>{
 
 
 
-let icons = document.querySelectorAll(".iconWithoutLink")
-console.log(icons)
+// let icons = document.querySelectorAll(".iconWithoutLink")
+// console.log(icons)
 
-let icons_div = document.querySelectorAll(".icons_div")
-console.log(icons_div)
+// let icons_div = document.querySelectorAll(".icons_div")
+// console.log(icons_div)
 
-let togle = true
-icons.forEach((el,ind)=>{
-    el.addEventListener("click" , ()=>{
-        RemoveClass()
+// let togle = true
+// icons.forEach((el,ind)=>{
+//     el.addEventListener("click" , ()=>{
+//         RemoveClass()
        
-        if(togle){
-            icons_div[ind].classList.remove("icon_div_hide")
-            togle = false
-        }else{
-            icons_div[ind].classList.add("icon_div_hide")
-            togle = true
-        }
+//         if(togle){
+//             icons_div[ind].classList.remove("icon_div_hide")
+//             togle = false
+//         }else{
+//             icons_div[ind].classList.add("icon_div_hide")
+//             togle = true
+//         }
         
-    })
-})
+//     })
+// })
 
 
-function RemoveClass (){
-    icons_div.forEach((el,ind)=>{
-        el.classList.add("icon_div_hide")
-    })
-}
+// function RemoveClass (){
+//     icons_div.forEach((el,ind)=>{
+//         el.classList.add("icon_div_hide")
+//     })
+// }
 
 
